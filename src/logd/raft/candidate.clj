a8d1129c90e48df6374069a6363fb24a600c796d
Logan Buckley <logan.buckley@gmail.com>
2017-03-11 10:17:33 -0500

    Express election timeout as action for candidate.clj

--- src/logd/raft/candidate.clj
+++ src/logd/raft/candidate.clj
@@ -1,11 +1,11 @@
 (ns logd.raft.candidate)
 
 (defn nominate-self [raft-state]
-  {:actions (for [peer peers]
-              [:request-vote peer])
+  {:actions (into [[:reset-election-timeout]]
+                  (for [peer (:peers raft-state)]
+                    [:request-vote peer]))
    :state (assoc raft-state
                  :role :candidate
-                 :election-timeout (new-election-timeout)
                  :current-term (inc (:current-term raft-state))
                  :requested-votes? false
                  :votes-received 1)}) ; vote for self automatically
