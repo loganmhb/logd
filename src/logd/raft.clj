60b698e46b59f1a23b4578096f642f35ac8ed98c
Logan Buckley <logan.buckley@gmail.com>
2017-03-11 10:06:57 -0500

    Delete some dead ends

--- src/logd/raft.clj
+++ src/logd/raft.clj
@@ -24,51 +24,14 @@
    :last-applied 0
    :peers #{peers}
    :role :follower
-   :election-timeout (new-election-timeout)
    ;; Candidate state for bookkeeping requesting votes
    :requested-votes? false
    :votes-received 0
-   :vote-response-chans [] ;; place to store channels for receiving votes
    ;; Leader state for tracking replication
    :replication-status (into {}
                              (for [peer peers]
                                {peer {:match-index 0
                                       :next-index 1}}))})
-
-(def rpc-chan (async/chan))
-
-(defn become-candidate [raft-state]
-  (assoc raft-state
-         :role :candidate
-         :election-timeout (new-election-timeout)
-         :current-term (inc (:current-term raft-state))
-         :requested-votes? false
-         :votes-received 1)) ; vote for self automatically
-
-(defn run-follower
-  "Waits for input or election timeout and returns the transitioned state."
-  [raft-state channels]
-  (async/alt!!
-    ;; Election timeout -- if no contact from leader, become candidate
-    (:election-timeout raft-state)
-    (become-candidate raft-state)
-    
-    ;; Handle an append-entries RPC call and update the state appropriately,
-    ;; resetting the election timer if the call was successful
-    (:append-entries channels)
-    ([rpc _]
-     (let [{:keys [response state]} (follower/append-entries raft-state (:data rpc))]
-       (async/>!! (:callback-chan rpc) response)
-       (if (:success response)
-         (assoc state :election-timeout (new-election-timeout))
-         state)))
-
-    ;; Handle a request-vote RPC call and update the state appropriately
-    (:request-vote channels)
-    ([rpc _]
-     (let [{:keys [response state]} (follower/request-vote raft-state (:data rpc))]
-       (async/>!! (:callback-chan rpc) response)
-       state))))
 
 
 (defn run-candidate [raft-state channels]
@@ -99,6 +62,3 @@
          (if (:success response)
            (assoc state :election-timeout (new-election-timeout))
            state))))))
-
-(defn run-leader [raft-state channels])
-
--- src/logd/raft/candidate.clj
+++ src/logd/raft/candidate.clj
@@ -1,3 +1,11 @@
 (ns logd.raft.candidate)
 
-(defn request-votes [raft-state])
+(defn nominate-self [raft-state]
+  {:actions (for [peer peers]
+              [:request-vote peer])
+   :state (assoc raft-state
+                 :role :candidate
+                 :election-timeout (new-election-timeout)
+                 :current-term (inc (:current-term raft-state))
+                 :requested-votes? false
+                 :votes-received 1)}) ; vote for self automatically
